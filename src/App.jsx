932c2127e6e46d23a38b50028c8d383d28d2745b
Poldon29 <s.banys11@gmail.com>
2022-05-17 11:18:05 +0200

    feat: :fire: Public version of app
    Initialized app with 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const OpenAlert = () =>{
+    alert('hello react')
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={OpenAlert}>Pop alert</button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a 
+          className="App-link" 
+          href="https://github.com/Poldon29/Front_End" 
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Git :)
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
